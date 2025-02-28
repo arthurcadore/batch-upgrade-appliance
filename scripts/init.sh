bb8b946b65f8c5062570cb25e3dc7c1178ea7237
arthurcadore <arthurbarcella.ifsc@gmail.com>
2025-02-28 11:55:11 -0300

    :sparkles: test update on main

--- scripts/init.sh
+++ scripts/init.sh
@@ -3,7 +3,6 @@
 # Definir variáveis
 DOCKER_COMPOSE_FILE="/home/cadore/batch-upgrade-appliance/docker-compose.yaml"
 
-# Verificar se o Docker está instalado
 echo "Verificando instalação do Docker..."
 if ! command -v docker &> /dev/null
 then
@@ -11,7 +10,6 @@ then
     exit 1
 fi
 
-# Criar redes MacVLAN no Docker
 echo "Criando redes MacVLAN no Docker..."
 docker network create -d macvlan \
   --subnet=10.10.10.0/24 \
@@ -28,8 +26,7 @@ docker network create -d macvlan \
   --gateway=10.10.10.1 \
   -o parent=vlan30 vlan30
 
-# Iniciar containers
 echo "Iniciando containers..."
-docker compose -f $DOCKER_COMPOSE_FILE up
+docker compose -f $DOCKER_COMPOSE_FILE up -d
 
 echo "Configuração concluída!"
