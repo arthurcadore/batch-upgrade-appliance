57692ec01dccf75115b9498acfaae99d2bdddead
arthurcadore <arthurbarcella.ifsc@gmail.com>
2025-02-28 11:43:06 -0300

    :sparkles: added macvlan on dockerfiles

--- scripts/init.sh
+++ scripts/init.sh
@@ -3,33 +3,7 @@
 # Definir variáveis
 DOCKER_COMPOSE_FILE="/home/cadore/batch-upgrade-appliance/docker-compose.yaml"
 
-# Criar namespaces de rede
-echo "Criando namespaces de rede..."
-sudo ip netns add tftp1
-sudo ip netns add tftp2
-sudo ip netns add tftp3
-
-# Mover VLANs para os namespaces
-echo "Movendo VLANs para os namespaces..."
-sudo ip link set vlan10 netns tftp1
-sudo ip link set vlan20 netns tftp2
-sudo ip link set vlan30 netns tftp3
-
-# Configurar IPs e rotas dentro dos namespaces
-echo "Configurando IPs e rotas..."
-sudo ip netns exec tftp1 ip addr add 10.10.10.2/24 dev vlan10
-sudo ip netns exec tftp1 ip link set vlan10 up
-sudo ip netns exec tftp1 ip route add default via 10.10.10.1 dev vlan10
-
-sudo ip netns exec tftp2 ip addr add 10.10.10.2/24 dev vlan20
-sudo ip netns exec tftp2 ip link set vlan20 up
-sudo ip netns exec tftp2 ip route add default via 10.10.10.1 dev vlan20
-
-sudo ip netns exec tftp3 ip addr add 10.10.10.2/24 dev vlan30
-sudo ip netns exec tftp3 ip link set vlan30 up
-sudo ip netns exec tftp3 ip route add default via 10.10.10.1 dev vlan30
-
-# Instalar Docker e Docker Compose se necessário
+# Verificar se o Docker está instalado
 echo "Verificando instalação do Docker..."
 if ! command -v docker &> /dev/null
 then
@@ -37,10 +11,25 @@ then
     exit 1
 fi
 
-# Rodar os containers dentro dos namespaces
+# Criar redes MacVLAN no Docker
+echo "Criando redes MacVLAN no Docker..."
+docker network create -d macvlan \
+  --subnet=10.10.10.0/24 \
+  --gateway=10.10.10.1 \
+  -o parent=vlan10 vlan10
+
+docker network create -d macvlan \
+  --subnet=10.10.10.0/24 \
+  --gateway=10.10.10.1 \
+  -o parent=vlan20 vlan20
+
+docker network create -d macvlan \
+  --subnet=10.10.10.0/24 \
+  --gateway=10.10.10.1 \
+  -o parent=vlan30 vlan30
+
+# Iniciar containers
 echo "Iniciando containers..."
-sudo ip netns exec tftp1 docker compose -f $DOCKER_COMPOSE_FILE up -d tftpserver1
-sudo ip netns exec tftp2 docker compose -f $DOCKER_COMPOSE_FILE up -d tftpserver2
-sudo ip netns exec tftp3 docker compose -f $DOCKER_COMPOSE_FILE up -d tftpserver3
+docker compose -f $DOCKER_COMPOSE_FILE up
 
 echo "Configuração concluída!"
--- scripts/netplan.sh
+++ scripts/netplan.sh
@@ -19,19 +19,19 @@ network:
       link: $ETH_INTERFACE
       macaddress: "00:11:22:33:44:10"
       dhcp4: no
-      addresses: []
+      addresses: [0.0.0.0/32]  # Adiciona um IP "nulo" para ativar a interface
     vlan20:
       id: 20
       link: $ETH_INTERFACE
       macaddress: "00:11:22:33:44:20"
       dhcp4: no
-      addresses: []
+      addresses: [0.0.0.0/32]
     vlan30:
       id: 30
       link: $ETH_INTERFACE
       macaddress: "00:11:22:33:44:30"
       dhcp4: no
-      addresses: []
+      addresses: [0.0.0.0/32]
 EOF
 
 # Aplicar Netplan
